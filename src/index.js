43a1b09d3eaa813dfba9dfaadd06aec987395276
Larry Villegas <larry_villegas@hotmail.com>
2023-04-07 13:03:50 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,51 @@
-import _ from 'lodash';
-import './style.css';
+import _ from "lodash";
+import "./style.css";
 
+let arrTasks = [
+  {
+    description: "Double-tap to edit",
+    completed: false,
+    index: 0,
+  },
+  {
+    description: "Drag 'n drop to reorder your list",
+    completed: true,
+    index: 1,
+  },
+  {
+    description: "Manage all your lists in one place",
+    completed: true,
+    index: 2,
+  },
+  {
+    description: "Resync to clear out the old",
+    completed: false,
+    index: 3,
+  },
+];
 
-
-function component() {
-  const element = document.createElement('div');
-  return element;
+function makeHtmlList() {
+  let html = '';
+  if(arrTasks.length===0) {
+   return `<h2>` 
+  }
+  for (let i = 0; i < arrTasks.length; i++) {
+  
+    html += `<li class="li-task">
+            <input class="complete" name="complete" type="checkbox" ${arrTasks[i].completed?'checked':''} >
+            <input class="txt-task" name="txt-task" type="text" value="${arrTasks[i].description}">
+            <button class="btn-drag"><i class="fa-solid fa-ellipsis-vertical"></i></button>
+            <button class="btn-remove"><i class="fa-regular fa-trash-can"></i></button>
+          </li>`
+    
+  }
+  return html;
 }
 
-document.body.appendChild(component());
+function showTasks() {
+  const ulTasks = document.getElementById('ul-tasks')
+  ulTasks.innerHTML = makeHtmlList();
+  console.log(ulTasks);
+}
+
+showTasks();
