5cd7ee04644b9b14d352d7804c84d6e909d1f086
Larry Villegas <larry_villegas@hotmail.com>
2023-04-07 02:16:38 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import _ from 'lodash';
 import './style.css';
 // import Icon from './logo.png';
 
+
 function component() {
   const element = document.createElement('div');
-
+  
   // Lodash, now imported by this script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   element.classList.add('hello');
