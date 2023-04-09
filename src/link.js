df7035191a7bb7aadaacf1b896634b03473fc9d1
Larry Villegas <larry_villegas@hotmail.com>
2023-04-09 01:08:37 -0400

    Create link.js

---
+++ src/link.js
@@ -0,0 +1,29 @@
+import TaskManager from './TaskManager.js';
+const taskManager = new TaskManager();
+
+export function addTask() {
+  const input = document.getElementById('new-task');
+  const description = input.value.trim();
+  if (description === '') {
+    return;
+  }
+  taskManager.addTask(description);
+  input.value = '';
+  return taskManager;
+}
+
+export function updateTask(index, inputElement) {
+  const description = inputElement.value.trim();
+  if (description === '') {
+    return;
+  }
+  taskManager.updateTask(index, description);
+}
+
+export function toggleTask(index){
+  taskManager.toggleTask(index);  
+}
+
+export function removeTask(index) {
+  taskManager.removeTask(index);
+}
