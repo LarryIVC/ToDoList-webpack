cd5e4263804ddde80e7334515c68e95e21bf7d66
Larry Villegas <larry_villegas@hotmail.com>
2023-04-08 20:02:10 -0400

    Update ClearAll.js

--- src/ClearAll.js
+++ src/ClearAll.js
@@ -1,10 +1,12 @@
-import renderTasks from './logic.js';
 /* eslint-disable */
+import renderTasks from './logic.js';
+
 export default function ClearAll(taskManager) {
   const bntClearAll = document.getElementById('btn-clear');
   bntClearAll.addEventListener('click', () => {
     taskManager.tasks = taskManager.tasks.filter((task) => !task.completed);
     taskManager.save();
-    renderTasks();
+    
+    renderTasks();    
   });
 }
