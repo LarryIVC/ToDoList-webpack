b49e28813def48447f93a4b74b4e7e9267e3f92d
Larry Villegas <larry_villegas@hotmail.com>
2023-04-09 02:40:06 -0400

    Update ClearAll.js

--- src/ClearAll.js
+++ src/ClearAll.js
@@ -1,4 +1,9 @@
-export default function ClearAll(taskManager) {
-  taskManager.tasks = taskManager.tasks.filter((task) => !task.completed);
-  taskManager.save();
+export default function ClearAll(taskManager, renderTasks) {
+  const bntClearAll = document.getElementById('btn-clear');
+  bntClearAll.addEventListener('click', () => {
+    taskManager.tasks = taskManager.tasks.filter((task) => !task.completed);
+    taskManager.save();
+    
+    renderTasks();    
+  });
 }
