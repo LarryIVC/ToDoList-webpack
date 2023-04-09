cd265bf8c210923215562b479c44ef248397d418
Larry Villegas <larry_villegas@hotmail.com>
2023-04-09 02:40:16 -0400

    Update logic.js

--- src/logic.js
+++ src/logic.js
@@ -1,14 +1,7 @@
-import ClearAll from './ClearAll.js';
 import TaskManager from './TaskManager.js';
-import {
-  addTask,
-  removeTask,
-  updateTask,
-  toggleTask,
-} from './link.js';
+import ClearAll from './ClearAll.js';
 
 const taskManager = new TaskManager();
-const bntClearAll = document.getElementById('btn-clear');
 
 export default function renderTasks() {
   const taskList = document.getElementById('ul-tasks');
@@ -21,18 +14,15 @@ export default function renderTasks() {
     input.classList.add('txt-task');
     input.addEventListener('focusout', () => {
       updateTask(index, input);
-      renderTasks();
     });
     const checkbox = document.createElement('input');
     listItem.classList.add('li-task');
     checkbox.classList.add('complete');
     checkbox.type = 'checkbox';
     checkbox.checked = task.completed;
-    checkbox.addEventListener('click', () => {
-      toggleTask(index);
-      renderTasks();
+    checkbox.addEventListener('click', (item) => {      
+      toggleTask(index);      
     });
-
     const removeButton = document.createElement('button');
     const trash = document.createElement('i');
     trash.classList.add('fa-regular');
@@ -40,30 +30,55 @@ export default function renderTasks() {
     removeButton.classList.add('btn-remove');
     removeButton.addEventListener('click', () => {
       removeTask(index);
-      renderTasks();
     });
-
-    if (task.completed) {
+    if(task.completed) {
       input.classList.add('completed');
-    } else {
+    } else 
+    {
       input.classList.remove('completed');
     }
     removeButton.appendChild(trash);
     listItem.appendChild(checkbox);
     listItem.appendChild(input);
     listItem.appendChild(removeButton);
-    taskList.appendChild(listItem);
+    taskList.appendChild(listItem);    
   });
+}
+
+function addTask() {
+  const input = document.getElementById('new-task');
+  const description = input.value.trim();
+  if (description === '') {
+    return;
+  }
+  taskManager.addTask(description);
+  input.value = '';
+  renderTasks();
+}
+
+function removeTask(index) {
+  taskManager.removeTask(index);
+  renderTasks();
+}
+
+function updateTask(index, inputElement) {
+  const description = inputElement.value.trim();
+  if (description === '') {
+    return;
+  }
+  taskManager.updateTask(index, description);
+  renderTasks();
+}
+
+function toggleTask(index){
+  taskManager.toggleTask(index);  
+  renderTasks();
 }
 
 const myForm = document.getElementById('frm-task');
 myForm.addEventListener('submit', (e) => {
   e.preventDefault();
-  taskManager.tasks = addTask();
-  renderTasks();
+  addTask();
 });
 
-bntClearAll.addEventListener('click', () => {
-  ClearAll(taskManager);
-  renderTasks();
-});
+ClearAll(taskManager, renderTasks);
