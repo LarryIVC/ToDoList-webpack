b93d200bd63aa986df67d13e45008aebdeeecb0d
Federica Ulzurrun <fzulzurrun@gmail.com>
2023-04-13 17:30:42 -0300

    add explaining comments

--- src/TaskActions.test.js
+++ src/TaskActions.test.js
@@ -1,12 +1,17 @@
+// Import TaskManager class and addTask and removeTask functions from TaskActions module
 import TaskManager from './TaskManager';
 import { addTask, removeTask } from './TaskActions';
+// Import the 'jest-localstorage-mock' package to mock the localStorage API
 import 'jest-localstorage-mock';
 
+// Test suite for addTask and removeTask functions
 describe('addTask & removeTask', () => {
+  // Clear localStorage before each test
   beforeEach(() => {
     localStorage.clear();
   });
 
+  // Test that addTask function adds a new task to the task manager
   test('should add a task to the task manager', () => {
     // Set up the task manager
     const taskManager = new TaskManager();
@@ -16,8 +21,11 @@ describe('addTask & removeTask', () => {
     input.value = 'Test task';
     document.body.appendChild(input); 
 
+    // Call addTask function
     addTask(taskManager, renderTasks);
 
+    // Assert that the new task has been added to the task manager, 
+    // renderTasks function has been called and the tasks have been saved to localStorage
     expect(taskManager.tasks).toContainEqual({ index: expect.any(Number), description: 'Test task', completed: false });
     expect(renderTasks).toHaveBeenCalled();
     expect(localStorage.setItem).toHaveBeenCalledWith(
@@ -25,9 +33,11 @@ describe('addTask & removeTask', () => {
       JSON.stringify(taskManager.tasks)
     );
 
+    // Clean up: remove input element from the document
     document.body.removeChild(input);
   });
 
+  // Test that removeTask function removes a task from the task manager
   test('removeTask should remove a task from the task manager', () => {
     // Set up the task manager
     const taskManager = new TaskManager();
@@ -36,6 +46,7 @@ describe('addTask & removeTask', () => {
     addTask(taskManager, jest.fn(), input);
     removeTask(taskManager, jest.fn(), 0);
 
+    // Assert that the task has been removed from the task manager
     expect(taskManager.tasks).toEqual([]);
   });
 });
